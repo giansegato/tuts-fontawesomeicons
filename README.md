66bf2fa3b71763fffe50e5f9da9bafacffea64d6
Gianluca Segato <gianluca.segato@gmail.com>
2015-08-13 21:55:21 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+### Tuts+ Tutorial: How to use FontAwesome in your Android Project
+
+#### Instructor: Gianluca Segato
+
+A sample project that shows how to use the FontAwesome icon pack on an Android project.
+
+**Availabe from 13/08/2015**
---
+++ app/src/main/java/com/tutsplus/code/fontawesomeicons/MainActivity.java
@@ -0,0 +1,20 @@
+package com.tutsplus.code.fontawesomeicons;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Let's mark this ViewGroup as an icon container
+        Typeface iconFont = FontManager.getTypeface(getApplicationContext(), FontManager.FONTAWESOME);
+        FontManager.markAsIconContainer(findViewById(R.id.icons_container), iconFont);
+
+    }
+
+}
